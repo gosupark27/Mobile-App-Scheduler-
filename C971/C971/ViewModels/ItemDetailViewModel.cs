306bd51b41d9bab6d608b383753ee3eb77d77b02
Heui Park <hpark41@student.wgu.edu>
2020-04-03 16:58:55 -0700

    Add project files.

---
+++ C971/C971/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using C971.Models;
+
+namespace C971.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item?.Text;
+			Item = item;
+		}
+	}
+}
