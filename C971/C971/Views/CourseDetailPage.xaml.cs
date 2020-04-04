e4334d646198d43f5c3d251d734acb35cd34bfd5
Heui Park <hpark41@student.wgu.edu>
2020-04-04 16:53:32 -0700

    Imported views from prevous Heui_Park C971 proj

---
+++ C971/C971/Views/CourseDetailPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace C971.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CourseDetailPage : ContentPage
+	{
+		public CourseDetailPage()
+		{
+			InitializeComponent();
+		}
+
+		async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			if (e.Item == null)
+				return;
+
+			await DisplayAlert("Item Tapped", "An item was tapped.", "OK");
+
+			//Deselect Item
+			((ListView)sender).SelectedItem = null;
+		}
+	}
+
+	
+}
